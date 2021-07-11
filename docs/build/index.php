651de00c3f2bbab67da0970954ad629fb0f8372f
Andrew Allsopp <arallsopp@gmail.com>
2021-07-12 00:16:46 +0100

    Writing readme via build

--- docs/build/index.php
+++ docs/build/index.php
@@ -18,24 +18,29 @@ update_readme($readme_file,$hue_icons,$custom_icons);
 
 function update_readme($readme_file,$hue_icons,$custom_icons){
     $readme = file_get_contents($readme_file);
-    $subst = '    | Icon | Name ' . PHP_EOL . '    | :--- | :--- |';
+
+    $subst = '(Start Hue Icons)' . PHP_EOL . PHP_EOL . '    | Icon | Name ' . PHP_EOL . '    | :--- | :--- |';
 
     //do the hue icons
-    $re = '/\[\/\/\]\: # \(Start Hue Icons\)(.*)\[\/\/\]\: # \(End Hue Icons\)/s';
-    foreach ($hue_icons as $hue_icon){
-        $subst .=  PHP_EOL . '| ![hue:' . $hue_icon->name . '](https://raw.githubusercontent.com/arallsopp/hass-hue-icons/main/docs/custom_svgs/' . $hue_icon->name . '.svg)| hue:' . $hue_icon->name . ' |';
+    $re = '/\(Start Hue Icons\).*\(End Hue Icons\)/s';
+    foreach ($hue_icons as $icon){
+        $subst .=  PHP_EOL . '    | ![hue:' . $icon->name . '](https://raw.githubusercontent.com/arallsopp/hass-hue-icons/main/docs/svgs/' . $icon->name . '.svg)| hue:' . $icon->name . ' |';
     }
+    $subst .= PHP_EOL . PHP_EOL . '[//]: # (End Hue Icons)';
     $readme = preg_replace($re, $subst, $readme);
 
+
+    $subst = '(Start Custom Icons)' . PHP_EOL . PHP_EOL . '    | Icon | Name ' . PHP_EOL . '    | :--- | :--- |';
 
     //do the custom icons
-    $subst = '    | Icon | Name ' . PHP_EOL . '    | :--- | :--- |';
-    $re = '/\[\/\/\]\: # \(Start Custom Icons\)(.*)\[\/\/\]\: # \(End Custom Icons\)/s';
-    foreach ($custom_icons as $custom_icon){
-        $subst .=  PHP_EOL . '| ![hue:' . $custom_icon->name . '](https://raw.githubusercontent.com/arallsopp/hass-hue-icons/main/docs/custom_svgs/' . $custom_icon->name . '.svg)| hue:' . $custom_icon->name . ' |';
+    $re = '/\(Start Custom Icons\).*\(End Custom Icons\)/s';
+    foreach ($custom_icons as $icon){
+        $subst .=  PHP_EOL . '    | ![hue:' . $icon->name . '](https://raw.githubusercontent.com/arallsopp/hass-hue-icons/main/docs/custom_svgs/' . $icon->name . '.svg)| hue:' . $icon->name . ' |';
     }
+    $subst .= PHP_EOL . PHP_EOL . '[//]: # (End Custom Icons)';
     $readme = preg_replace($re, $subst, $readme);
 
+    echo '<pre>' . $readme . '</pre>';
     file_put_contents($readme_file,$readme);
 }
 
@@ -51,16 +56,18 @@ function read_files($path) {
 // print.
     foreach ($files as $file) {
         $name = basename($file, '.svg');
-        $content = file_get_contents($file);
-        $re = '/<path d\s?=\s?"(.*)"\/>/mis';
-        preg_match_all($re, $content, $matches, PREG_SET_ORDER, 0);
+        if($name != 'blank') {
+            $content = file_get_contents($file);
+            $re = '/<path d\s?=\s?"(.*)"\/>/mis';
+            preg_match_all($re, $content, $matches, PREG_SET_ORDER, 0);
 
-        $item = new stdClass();
-        $item->name = $name;
-        $item->content = $matches[0][1];
+            $item = new stdClass();
+            $item->name = $name;
+            $item->content = $matches[0][1];
 
-        var_dump($item);
-        array_push($items,$item);
+            var_dump($item);
+            array_push($items, $item);
+        }
     }
     return $items;
 }
