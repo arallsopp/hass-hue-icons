5faef118d797618c2b8c3c9f10b027fa3111e16c
Andy Allsopp <arallsopp@gmail.com>
2022-05-25 00:48:58 +0100

    angular tester now able to read from script and create a dropdown.

--- docs/build/tester/script.js
+++ docs/build/tester/script.js
@@ -1,8 +1,71 @@
-"use strict";
-var app = angular.module("tester", ['ngMaterial', 'ngMessages']);
+(function () {
+    'use strict';
+    angular
+        .module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+        .controller('DemoCtrl', DemoCtrl);
 
-app.controller('AppCtrl', ['$scope', '$http','$mdToast',
-    function($scope, $http, $mdToast) {
-        $scope.message = 'hello world';
+    function DemoCtrl ($timeout, $http, $q, $log) {
+        var self = this;
+
+        self.icons = importFromScript();
+        self.querySearch   = querySearch;
+        self.selectedItemChange = selectedItemChange;
+        self.searchTextChange   = searchTextChange;
+
+        // ******************************
+        // Internal methods
+        // ******************************
+
+        /**
+         * Search for icons.
+         */
+        function querySearch (query) {
+            let results = query ? self.icons.filter(createFilterFor(query)) : self.icons;
+            return results;
+        }
+
+        function searchTextChange(text) {
+            $log.info('Text changed to ' + text);
+        }
+
+        function selectedItemChange(item) {
+            $log.info('Item changed to ' + JSON.stringify(item));
+        }
+
+        /**
+         * Build `components` list of key/value pairs
+         */
+        function importFromScript() {
+            let icons = [];
+            for (const icon in HUE_ICONS_MAP){
+
+                let keywords = HUE_ICONS_MAP[icon].keywords,
+                    aliases = keywords.join(', ');
+
+                icons.push({
+                    name: icon,
+                    srcfile:'../../custom_svgs/' + icon + '.svg',
+                    path:HUE_ICONS_MAP[icon].path,
+                    keywords:keywords,
+                    aliases:aliases,
+                    value:'hue:' + icon + ' ' + aliases.toLowerCase()
+                });
+            }
+            return icons;
+        }
+
+        /**
+         * Create filter function for a query string
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = query.toLowerCase();
+
+            return function filterFn(item) {
+                return (item.value.indexOf(lowercaseQuery) != -1);
+            };
+
+        }
     }
-]);
+})();
+
+
