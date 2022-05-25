b680231d5ccaffded6c07780cec4d5861f8549f0
Andy Allsopp <arallsopp@gmail.com>
2022-05-25 01:47:37 +0100

    now lets you use args to showcase an icon at startup (?search=adore)

--- docs/build/tester/script.js
+++ docs/build/tester/script.js
@@ -4,15 +4,21 @@
         .module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
         .controller('DemoCtrl', DemoCtrl);
 
-    function DemoCtrl ($scope, $timeout, $http, $q, $log) {
+    function DemoCtrl ($scope, $q, $log) {
 
-        $scope.selectedIcon = false;
         var self = this;
 
         self.icons = importFromScript();
         self.querySearch   = querySearch;
         self.selectedItemChange = selectedItemChange;
         self.searchTextChange   = searchTextChange;
+
+        // set up current icon if provided
+        let params = new URLSearchParams(document.location.search);
+        $scope.selectedIcon = params.get('search')
+           ? self.icons.filter(createFilterFor(params.get('search')))[0]
+           : false;
+
 
         // ******************************
         // Internal methods
